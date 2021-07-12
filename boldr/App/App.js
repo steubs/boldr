36dbfe18678a0411217de39cb4c684acbd3d1bbe
Josh Steubs <joshua.steubs@gmail.com>
2021-07-12 15:29:47 -0700

    add vid player, styled-components

---
+++ boldr/App/App.js
@@ -0,0 +1,11 @@
+import { Info } from './AppStyles';
+import ReactPlayer from 'react-player';
+function App() {
+  return (
+    <div className="App">
+      <ReactPlayer url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
+    </div>
+  );
+}
+
+export default App;
---
+++ boldr/App/AppStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Info = styled.h1`
+  
+  width: 100%;
+  padding: 0 50px;
+  color: black;
+  font-style: 2rem;
+  line-height: 24px;
+  text-align: justify;
+ 
+}
+`;
