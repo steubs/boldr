112158d9b2413de8b4096fd013f1430a5960d71f
Josh Steubs <joshua.steubs@gmail.com>
2021-07-12 18:08:59 -0700

    getting BlazePose to work

--- boldr/App/App.js
+++ boldr/App/App.js
@@ -1,9 +1,34 @@
 import { Info } from './AppStyles';
 import ReactPlayer from 'react-player';
-function App() {
+import '@tensorflow/tfjs-backend-webgl'
+import * as poseDetection from '@tensorflow-models/pose-detection'
+import { Pose, POSE_CONNECTIONS, LandmarkGrid, PoseConfig, landmarkContainer } from '@mediapipe/pose'
+import { useState, useEffect } from 'react';
+
+const App = () => {
+
+  const [detector, setDetector] = useState();
+  const [video, setVideo] = useState();
+  const [poses, setPoses] = useState([]);
+  const [pose, setPose] = useState([0]);
+
+  useEffect(() => {
+    //TODO: read about this
+    //https://reactjs.org/docs/concurrent-mode-suspense.html
+    async function buildPose() {
+      setDetector(await poseDetection.createDetector(poseDetection.SupportedModels.BlazePose, { runtime: 'tfjs' }))
+      setVideo(document.getElementById('video'));
+      setPoses([...await detector.estimatePoses(video)]);
+      setPose([...poses[0].keypoints])
+    }
+    buildPose();
+  }, [])
   return (
     <div className="App">
-      <ReactPlayer url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
+      <ReactPlayer url='https://www.youtube.com/watch?v=dQw4w9WgXcQ&html5=True' />
+      <Info>
+        Coords: {pose[0]}
+      </Info>
     </div>
   );
 }
